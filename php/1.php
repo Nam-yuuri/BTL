1f2ea10e33b8abddd4276f45dc4fc9ff7d33d16a
Admin <admin@vinasupport.com>
2021-01-12 00:59:59 +0700

    BTL

--- php/1.php
+++ php/1.php
@@ -1,106 +1,24 @@
 <!doctype html>
 <html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="../css/home.css">
-</head>
-
-<body>
-  <style>
-     @media (max-width:902px){
-       .col-lg-2{
-         height:0px;
-       }
-     }
-  </style>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-lg-2">
-        <?php include("right.php");?>
-      </div>
-      </div>
-      <div class="col-lg-10">
-        <section>
-        <div class="bark container-fluid"
-          style="background:url(../image/web.jpg) no-repeat center  center;background-size:cover;width:100%;height;100% ">
-          <div class="main-wrapper" style="text-align: center;font-size:200%">
-            <p class="cd-headline clip is-full-width">
-              <span>language learned</span> <span class="typer" data-colors="#1ed373" id="main"
-                data-words="C++,C#,PYTHON,SQL,PHP" data-delay="100" data-deletedelay="1000"
-                style="color: rgb(30, 211, 115);">
-              </span>
-            </p>
-          </div>
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-lg-12" >
-                <div class="carousel slide" id="carousel-176652">
-                  <ol class="carousel-indicators">
-                    <li data-slide-to="0" data-target="#carousel-176652" class="active">
-                    </li>
-                    <li data-slide-to="1" data-target="#carousel-176652">
-                    </li>
-                    <li data-slide-to="2" data-target="#carousel-176652">
-                    </li>
-                    <li data-slide-to="3" data-target="#carousel-176652">
-                    </li>
-                    <li data-slide-to="4" data-target="#carousel-176652">
-                    </li>
-                  </ol>
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img class="d-block w-100" alt="Carousel Bootstrap First" src="../image/C++.jpg ">
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" alt="Carousel Bootstrap Second" src="../image/C.jpg" />
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" alt="Carousel Bootstrap Third" src="../image/python.jpg" />
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" alt="Carousel Bootstrap Third" src="../image/sql.jpg" />
-                    </div>
-                    <div class="carousel-item">
-                      <img class="d-block w-100" alt="Carousel Bootstrap Third" src="../image/php.jpg" />
-                    </div>
-                    <a class="carousel-control-prev" style="width: 100px;" href="#carousel-176652"
-                    data-slide="prev"><span class="carousel-control-prev-icon"></span> <span
-                      class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-176652"
-                    data-slide="next"><span class="carousel-control-next-icon"></span> <span
-                      class="sr-only">Next</span></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        </section>
-      </div>
-      <?php include("left.php");?>
-
-
-    </div>
-  </div>
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-</body>
-
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="case-item" style="height: 190.2px;">
+								    <img class="case-item__icon" src="../assets/icons/icon-design.svg" alt="">
+								    <h3 class="title title--h3">Web Design</h3>
+								    <p class="case-item__caption">The most modern and high-quality design made at a professional level. </p>
+							    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
--- php/aboutnam.php
+++ php/aboutnam.php
@@ -8,25 +8,155 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   </head>
   <body>
-      <div class="containernam" style="background:url(../image/background-nam.jpg) no-repeat center  center; background-size:cover;">
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
-        <h1>kjsbdfkhb</h1>
+  <style>
+    .col-lg-5{
+      margin:5px 5px 5px 5px;
+    }
+    .col-lg-4{
+      margin:20px 20px 20px 20px;
+    }
+    .col-lg-5,.col-lg-3,img{
+    border-radius: 20px;
+    }
+    .img{
+      box-shadow: none;
+      background-color: transparent;
+      padding: 0;
+      margin-top:20px;
+    }
+    /* img{
+      width:100%;
+      height:100%;
+    } */
+  </style>
+  <div class="container-fluid bg-white" >
+    <div class="container bg-dark" >
+      <div class="row information">
+        <div class="col-lg-12 text-center text-white"><h1>About Me</h1></div>
+          
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-lg-3 ">
+                <div class="img">
+                  <img src="../image/minh.jpg" alt="">
+                </div>
+              </div>
+              <div class="col-lg-5 text-white">
+              <h3>cái này để đây làm sau</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div class="container bg-dark">
+        <div class="row skill">
+          <div class="col-lg-6 text-white">
+            <h2>information</h2>
+            <ul class="list">
+                    <li><p>Birthday: 25/07/2000</p></li>
+                    <li><p>Phone: 0395001595</p></li>
+                    <li><p>Address: Thanh Hoa</p></li>
+                    <li><p>Mail: <a href="hoangvohoainamvtath5@gmail.com">hoangvohoainamvtath5@gmail.com</a></p></li>
+                    <li><p>Age: 20</p></li>
+                    <li><p>Degree: student</p></li>
+                    <li><p>Branch: Information Technology</p></li>
+            </ul>
+          </div>
+          <div class="col-lg-6 text-white">
+            <h2>Professional Skills</h2>
+                    <span>C++</span><span style="float:right">50%</span>
+                    <div class="progress">
+                      <div class="progress-bar progress-bar-striped progress-bar-danger active" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+                        50%
+                      </div>
+                    </div>
+                    <span>C#</span><span style="float:right">65%</span>
+                    <div class="progress">
+                      <div class="progress-bar progress-bar-striped progress-bar-info active" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width:65%">
+                        65%
+                      </div>
+                    </div>
+                    <span>Python</span><span style="float:right">60%</span>
+                    <div class="progress">
+                      <div class="progress-bar progress-bar-striped progress-bar-warning active" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:60%">
+                        60%
+                      </div>
+                    </div>
+                    <span>SQL</span><span style="float:right">75%</span>
+                    <div class="progress">
+                      <div class="progress-bar progress-bar-striped progress-bar-success active" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:75%">
+                        75%
+                      </div>
+                    </div>
+                    <span>PHP</span><span style="float:right">75%</span>
+                    <div class="progress">
+                      <div class="progress-bar progress-bar-striped progress-bar-success active" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width:75%">
+                        75%
+                      </div>
+                    </div>
+          </div>
+        </div>
+          <div class="row work">  
+            <div class="col-lg-5 bg-white">
+            <div class="service-box">
+					                            <div class="service-icon">
+					                                <i class="fa fa-smile-o"></i>
+					                            </div>
+					                            <div class="service-content">
+					                                <h3>Creative Design</h3>
+					                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+					                            </div>
+			
+            </div>
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-5  bg-white">
+            <div class="service-box">
+					                            <div class="service-icon">
+					                                <i class="fa fa-smile-o"></i>
+					                            </div>
+					                            <div class="service-content">
+					                                <h3>Creative Design</h3>
+					                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+					                            </div>
+			
+            </div>
+            </div>
+            <div class="col-lg-5  bg-white">
+            <div class="service-box">
+					                            <div class="service-icon">
+					                                <i class="fa fa-smile-o"></i>
+					                            </div>
+					                            <div class="service-content">
+					                                <h3>Creative Design</h3>
+					                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+					                            </div>
+			
+            </div>
+            </div>
+            <div class="col-lg-1"></div>
+            <div class="col-lg-5  bg-white">
+            <div class="service-box">
+					                            <div class="service-icon">
+					                                <i class="fa fa-smile-o"></i>
+					                            </div>
+					                            <div class="service-content">
+					                                <h3>Creative Design</h3>
+					                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+					                            </div>
+			
+            </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
