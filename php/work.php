9e6f0bd4cd81e953f455662328b3e122b8825e70
hnimnd1304 <1851060572@e.tlu.edu.vn>
2021-01-13 18:30:30 +0700

    Them work

---
+++ php/work.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../css/work.css">
+</head>
+
+<?php include("header.php");?>
+
+<body style="background:url(../image/background-work.jpg) no-repeat center  center; background-size:cover;height:100vh">
+
+<div class="container-fluid" >
+        <div class="col-lg-10">
+          <div class="container-fluid" >
+              <div class="target">
+                <div class="row" >
+                  <div class="col-sm-6" > <a href="../image/minh.jpg"><span
+                    class="glyphicon glyphicon-log-in"></span><img src="../image/minh.jpg" style="margin-bottom:30px" alt=""></a>
+                    <h3>Hoàng Võ Hoài Nam</h3>
+                    <a href="worknam.php"><button type="button" style="margin-top:30px" class="btn btn-outline-info">Tìm Hiểu</button></a>
+                  </div>
+                  <div class="col-sm-6" > <a href="../image/minh.jpg"><span
+                    class="glyphicon glyphicon-user"></span><img src="../image/minh.jpg" style="margin-bottom:30px" alt=""></a>
+                    <h3>Đào Nhật Minh</h3>
+                    <a href="workminh.php"><button type="button" style="margin-top:30px" class="btn btn-outline-info">Tìm Hiểu</button></a>
+                  </div>                    
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("footer.php");?>
