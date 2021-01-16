d3e9099e08686c54c7ea9e4fb0eeccd6dcc8f2dc
hnimnd1304 <1851060572@e.tlu.edu.vn>
2021-01-16 23:01:05 +0700

    Push lai tu nhanh test

--- php/login.php
+++ php/login.php
@@ -1,112 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>CV</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-  <style>
-body {font-family: Arial, Helvetica, sans-serif;}
-form {border: 3px solid #f1f1f1;}
+<?php
+    session_start();
+    include "config.php";
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        function validate($data){
+            $data = trim($data);
+            $data = stripcslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
 
-input[type=text], input[type=password] {
-  width: 100%;
-  padding: 12px 20px;
-  margin: 8px 0;
-  display: inline-block;
-  border: 1px solid #ccc;
-  box-sizing: border-box;
-}
+        $user = validate($_POST['username']);
+        $pass = validate($_POST['password']);
 
-button {
-  background-color: #4CAF50;
-  color: white;
-  padding: 14px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-}
-
-button:hover {
-  opacity: 0.8;
-}
-
-.cancelbtn {
-  width: auto;
-  padding: 10px 18px;
-  background-color: #f44336;
-}
-
-.imgcontainer {
-  text-align: center;
-  margin: 24px 0 12px 0;
-}
-
-img.avatar {
-  width: 40%;
-  border-radius: 50%;
-}
-
-.container {
-  padding: 16px;
-}
-
-span.psw {
-  float: right;
-  padding-top: 16px;
-}
-
-/* Change styles for span and cancel button on extra lgall screens */
-@media screen and (max-width: 300px) {
-  span.psw {
-     display: block;
-     float: none;
-  }
-  .cancelbtn {
-     width: 100%;
-  }
-}
-</style>
-</head>
-<body>
-
-<div class="container-fluid">
-  
-  <div class="row">
-    <div class="col-lg-4" ></div>
-    <div class="col-lg-4" >
-        <form action="/action_page.php" method="post">
-            <div class="imgcontainer">
-                <img src="../image/avatar.jpg" alt="Avatar" class="avatar">
-            </div>
-
-            <div class="container-fluid">
-                <label for="uname"><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" required>
-
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required>
-                    
-                <button type="submit">Login</button>
-                <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-                </label>
-            </div>
-
-            <div class="container-fluid" >
-                <a href="Home.php"><button type="button" class="cancelbtn">Cancel</button></a>
-                <span class="psw">Forgot <a href="#">password?</a></span>
-            </div>
-        </form>
-
-    </div>
-</div>
-    <div class="col-lg-4" ></div>
-  </div>
-</div>
-
-</body>
-</html>
+        if(empty($user)){
+            header("location: index_login.php?error=Username is requied");
+            exit();
+        }else if(empty($pass)){
+            header("location: index_login.php?error=Password is requied");
+            exit();
+        }else{
+            $sql = "SELECT * FROM admin WHERE username = '$user' AND password = '$pass'";
+            $result = mysqli_query($conn, $sql);
+            if(mysqli_num_rows($result) === 1){
+                $row = mysqli_fetch_assoc($result);
+                if($row['username'] == $user && $row['password'] == $pass){
+                    $_SESSION['username'] = $row['username'];
+                    header('location: index_loginlogin.php');
+                    exit();
+                }else{
+                    header("location: index_login.php?error=Incorrect username or password");
+                    exit();
+                }
+            }else{
+                header("location: index_login.php?error=Incorrect username or password");
+                exit();
+            }
+        }
+    }else{
+        header('location: index_login.php');
+        exit();
+    }
+?>
